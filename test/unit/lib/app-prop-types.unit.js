f8ec60be607da8f47fe8daa2224f3f5bea1eb8e4
Emanuele Busi <emanuele.busi@mondora.com>
2015-07-10 09:23:13 +0200

    #80 added few unit tests

--- test/unit/lib/app-prop-types.unit.js
+++ test/unit/lib/app-prop-types.unit.js
@@ -24,3 +24,33 @@ describe("The `DateEquivalent` function", function () {
     // });
 
 });
+
+describe("The DygraphCoordinate method", function () {
+
+    it("should not return an error if the prop is a DygraphCoordinate format", function () {
+        var case1 = [new Date(), [1, 2]];
+        var case2 = [new Date(), [1, 2], [3, 4]];
+        var props = {coordinates: case1};
+        expect(AppPropTypes.DygraphCoordinate(props, "coordinates")).to.be.null;
+        props = {coordinates: case2};
+        expect(AppPropTypes.DygraphCoordinate(props, "coordinates")).to.be.null;
+    });
+
+    it("should return an error if the first element of the coordinates is not a Date", function () {
+        var case1 = ["2015-01-01", [1, 2]];
+        var props = {coordinates: case1};
+
+        var validationResult = AppPropTypes.DygraphCoordinate(props, "coordinates");
+        expect(validationResult).to.be.an.instanceOf(Error);
+        expect(validationResult.message).to.contains("must be a date");
+    });
+
+    it("should return an error if the other elements are not arrays", function () {
+        var case1 = [new Date(), 1, 2];
+        var props = {coordinates: case1};
+
+        var validationResult = AppPropTypes.DygraphCoordinate(props, "coordinates");
+        expect(validationResult).to.be.an.instanceOf(Error);
+        expect(validationResult.message).to.contains("must be a 2-tuple of numbers");
+    });
+});
