56e2be421524f140d8244c55b385775ae2d3216f
Davide Bianchi <davide.bianchi@mondora.com>
2015-11-11 17:21:30 +0100

    IWWA-103 bug fix to first zoom.

--- app/components/temporal-line-graph/index.jsx
+++ app/components/temporal-line-graph/index.jsx
@@ -108,7 +108,7 @@ var TemporalLineGraph = React.createClass({
         }
         if (props.dateWindow) {
             options.dateWindow = props.dateWindow;
-        } else if (!R.isNil(props.dateFilter)) {
+        } else if (props.dateWindow && props.dateFilter) {
             options.dateWindow = [props.dateFilter.start, props.dateFilter.end];
         } else {
             const {max, min} = props.coordinates.reduce((acc, coordinate) => {
