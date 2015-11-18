3eee7116731b9bf5030d81cb04b8606d5c9c2862
Davide Bianchi <davide.bianchi@mondora.com>
2015-11-18 16:38:23 +0100

    corrected graphic bug in width

--- app/components/temporal-line-graph/index.jsx
+++ app/components/temporal-line-graph/index.jsx
@@ -12,7 +12,7 @@ var colors           = require("lib/colors");
 
 var styles = {
     graphContainer: {
-        width: ENVIRONMENT === "cordova" ? "calc(100vw - 100px)" : `calc(100vw - 165px)`,
+        width: ENVIRONMENT === "cordova" ? "calc(100vw - 100px)" : `calc(100vw - 130px)`,
         height: "calc(100vh - 400px)",
         margin: "20px 20px 30px 0px"
     }
