f6d5a9bbee68a64610fca2a58dbb4805d203cde4
Davide Bianchi <davide.bianchi@mondora.com>
2016-01-22 18:22:42 +0100

    Added decimate function.

--- app/lib/readings-daily-aggregates-to-dygraph-data.js
+++ app/lib/readings-daily-aggregates-to-dygraph-data.js
@@ -1,16 +1,28 @@
 import moment from "moment";
-import {findIndex, prop, sortBy, values} from "ramda";
+import {
+    addIndex,
+    all,
+    findIndex,
+    identity,
+    last,
+    memoize,
+    prop,
+    reduce,
+    sortBy,
+    values
+} from "ramda";
 
-var lastNotNull = 0.01;
+const EMPTY = Symbol("EMPTY");
+const ALMOST_ZERO = 0.01;
 
 function getFilterFn (filter) {
-    return aggregate => (
+    return memoize(aggregate => (
         aggregate.get("sensorId") === filter.sensorId &&
         aggregate.get("source") === filter.source &&
         aggregate.get("measurementType") === filter.measurementType &&
         (filter.date.start ? moment(aggregate.get("day")).isSameOrAfter(filter.date.start) : true) &&
         (filter.date.end ? moment(aggregate.get("day")).isSameOrBefore(filter.date.end) : true)
-    );
+    ));
 }
 
 function getFindAggregateFilterIndex (filters) {
@@ -19,7 +31,7 @@ function getFindAggregateFilterIndex (filters) {
 }
 
 function groupByDate (filters) {
-    const defaultGroup = filters.map(() => [lastNotNull]);
+    const defaultGroup = filters.map(() => [EMPTY]);
     const findAggregateFilterIndex = getFindAggregateFilterIndex(filters);
     return (group, aggregate) => {
         const index = findAggregateFilterIndex(aggregate);
@@ -30,23 +42,90 @@ function groupByDate (filters) {
         const measurementsDeltaInMs = aggregate.get("measurementsDeltaInMs");
         const measurementValuesArray = aggregate.get("measurementValues").split(",");
         measurementValuesArray.forEach((value, offset) => {
-            if (filters.length <= 1 && isNaN(parseFloat(value))) {
-                return group;
-            }
             const date = day + (offset * measurementsDeltaInMs);
             group[date] = group[date] || [new Date(date)].concat(defaultGroup);
-            if (parseFloat(value) === 0 || isNaN(parseFloat(value))) {
-                group[date][index + 1] = [lastNotNull];
-            } else {
-                lastNotNull = parseFloat(value);
-                group[date][index + 1] = [parseFloat(value)] || [lastNotNull];
-            }
+            const numericValue = parseFloat(value);
+            group[date][index + 1] = [
+                numericValue ||
+                (numericValue === 0 ? ALMOST_ZERO : EMPTY)
+            ];
         });
         return group;
     };
 }
 
-export default function readingsDailyAggregatesToDygraphData (aggregates, filters) {
-    const group = aggregates.reduce(groupByDate(filters), {});
-    return sortBy(prop(0), values(group));
+
+
+/*
+*   Decimation algorithm
+*   TODO move to a different file
+*/
+
+function isInRange (val1, val2) {
+    return (
+        val1 * 1.10 > val2 &&
+        val1 * 0.90 < val2
+    );
 }
+
+function areInRange (values1, values2) {
+    const results = [];
+    for (var i=0; i< values1.length; i++) {
+        results.push(
+            isInRange(values1[i], values2[i])
+        );
+    }
+    return all(identity, results);
+}
+
+function getValues (dataPoint) {
+    return dataPoint.slice(1).map(p => p[0]);
+}
+
+const indexedReduce = addIndex(reduce);
+
+function decimate (dygraphData) {
+    const length = dygraphData.length;
+    return indexedReduce((acc, dataPoint, index) => {
+        const needsToAdd = (
+            index === 0 ||
+            index === length - 1 ||
+            !areInRange(getValues(last(acc)), getValues(dataPoint)) ||
+            index % 24 === 12
+        );
+        if (needsToAdd) {
+            acc.push(dataPoint);
+        }
+        return acc;
+    }, [], dygraphData);
+}
+
+/*
+*   Decimation algorithm - END
+*   TODO move to a different file
+*/
+
+
+
+function fillMissingData (dygraphData) {
+    return reduce((acc, dataPoint) => {
+        const newDataPoint = [dataPoint[0]];
+        dataPoint.slice(1).forEach(([value], index) => {
+            newDataPoint[index + 1] = [
+                value === EMPTY ?
+                (last(acc) ? last(acc)[index + 1][0] : ALMOST_ZERO) :
+                value
+            ];
+        });
+        acc.push(newDataPoint);
+        return acc;
+    }, [], dygraphData);
+}
+
+export default memoize(function readingsDailyAggregatesToDygraphData (aggregates, filters) {
+    const group = aggregates.reduce(groupByDate(filters), {});
+    const filledData = fillMissingData(
+        sortBy(prop(0), values(group))
+    );
+    return decimate(filledData);
+});
