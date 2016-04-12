ddf50150469f5fe7ddf5ea32bb877f8769816bc0
Emanuele Busi <emanuele.busi@mondora.com>
2016-04-12 21:12:30 +0200

    Actions and reducers for zoom and dateFilters

--- app/actions/monitoring-chart.js
+++ app/actions/monitoring-chart.js
@@ -1,8 +1,15 @@
+import {Number, String, struct, list} from "tcomb";
+
+import actionTypeValidator from "../lib/action-type-validator";
+
 export const ADD_TO_FAVORITE = "ADD_TO_FAVORITE";
 export const CHANGE_Y_AXIS_VALUES = "CHANGE_Y_AXIS_VALUES";
 export const SAVE_CHART_CONFIG = "SAVE_CHART_CONFIG";
 export const SELECT_CHART_TYPE = "SELECT_CHART_TYPE";
 export const SELECT_FAVORITE_CHART = "SELECT_FAVORITE_CHART";
+export const SELECT_DATE_RANGES = "SELECT_DATE_RANGES";
+export const SET_ZOOM_EXTREMES = "SET_ZOOM_EXTREMES";
+export const RESET_ZOOM = "RESET_ZOOM";
 
 export const addToFavorite = (config) => {
     return {
@@ -38,3 +45,39 @@ export const selectFavoriteChart = (favoriteChart) => {
         payload: favoriteChart.get("config")
     };
 };
+
+const typeofSelectDateRanges = actionTypeValidator(
+    list(
+        struct({
+            key: String,
+            label: String
+        })
+    )
+);
+export function selectDateRanges (dateRange) {
+    typeofSelectDateRanges(dateRange);
+    return {
+        type: SELECT_DATE_RANGES,
+        payload: dateRange
+    };
+}
+
+const typeOfSetZoomExtremes = actionTypeValidator(list(
+    struct({
+        max: Number,
+        min: Number
+    })
+));
+export function setZoomExtremes (zoomExtremes) {
+    typeOfSetZoomExtremes(...arguments);
+    return {
+        type: SET_ZOOM_EXTREMES,
+        payload: zoomExtremes
+    };
+}
+
+export function resetZoom () {
+    return {
+        type: RESET_ZOOM
+    };
+}
