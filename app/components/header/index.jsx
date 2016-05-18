1564eb6ef35b8167a0131a61d5961d03201e4e2a
Emanuele Busi <emanuele.busi@mondora.com>
2016-05-18 16:29:31 +0200

    Created roles utils. Hidden Monitoring for no `admin` or `yousave` users

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -5,6 +5,7 @@ import {merge, prop} from "ramda";
 
 import {defaultTheme} from "lib/theme";
 import components from "components";
+import {isAdmin} from "lib/roles-utils";
 
 var stylesFunction = ({colors}) => ({
     base: {
@@ -47,11 +48,6 @@ var Header = React.createClass({
     logout: function () {
         this.props.asteroid.logout();
     },
-    userIsAdmin: function () {
-        const users = this.props.asteroid.collections.get("users") || Map();
-        const roles = users.getIn([this.props.asteroid.userId, "roles"]) || List();
-        return roles.includes("admin");
-    },
     getUserSettings: function () {
         return [
             {label: "Tema scuro", key: "dark"},
@@ -87,7 +83,7 @@ var Header = React.createClass({
         );
     },
     renderAdminPage: function () {
-        return this.userIsAdmin() && ENVIRONMENT !== "cordova" ? (
+        return isAdmin(this.props.asteroid) && ENVIRONMENT !== "cordova" ? (
             <span style={{marginRight: "15px"}}>
                 <Link to="/users/" >
                     <components.Icon
