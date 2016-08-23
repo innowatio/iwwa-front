6f5353b294a43367d7d4fb5545db7a70c70f2adc
Manuel Serra <nitroin@hotmail.com>
2016-08-23 10:11:12 +0200

    Changed logout function

--- app/views/root/index.jsx
+++ app/views/root/index.jsx
@@ -9,7 +9,6 @@ import components from "components";
 
 import asteroid from "lib/asteroid";
 import {EXEC_ENV} from "lib/config";
-import {setTokenOnInnowatioSSO} from "lib/innowatio-sso";
 import LocalStorageMixin from "lib/localstorage-mixin";
 import measures from "lib/measures";
 import {isAdmin, isYousaveUser, isAuthorizedUser} from "lib/roles-utils";
@@ -83,7 +82,7 @@ var Root = React.createClass({
         asteroid.subscribe("users");
     },
     logout: function () {
-        setTokenOnInnowatioSSO(null, asteroid.logout.bind(asteroid));
+        asteroid.logout();
     },
     getTheme: function () {
         const colorTheme = this.props.reduxState.userSetting.theme.color || "dark";
