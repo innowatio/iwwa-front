91bfaccd41ca26162ee5c74ce9c8a812b242f466
francesca <francesca.giudici@mondora.com>
2017-01-09 12:18:30 +0100

    [IWWA-889]: add icon used in filter component

--- app/lib/icons.js
+++ app/lib/icons.js
@@ -2,4 +2,5 @@ var assetsPathTo = require("lib/assets-path-to");
 
 exports.iconArrowRightWhite     = assetsPathTo("icons/arrow-right.svg");
 exports.iconArrowLeft           = assetsPathTo("icons/arrow-left.svg");
+exports.iconArrowUpDown         = assetsPathTo("icons/arrow-up-down.svg");
 exports.iconGoGreen             = assetsPathTo("icons/ico_gogreen.svg");
